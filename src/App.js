d2a8b7ad1158448ee4ac2cb697e89dcfbe0b0a96
Your Name <you@example.com>
2020-08-09 12:51:29 +0300

    Sign in

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import {BrowserRouter,Switch ,Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
-const HatsPage = () =>(
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
+
 
 function App() {
   return (
@@ -20,6 +17,7 @@ function App() {
       <Route exact path='/' component={HomePage}/>
 
       <Route  path='/shop' component={ShopPage}/>
+      <Route  path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
       </BrowserRouter>
       </div>
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../components/sing-in/sing-in.component.jsx';
+
+const SignInAndSignUpPage = ()=>(
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUpPage;
